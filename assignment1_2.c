ecbc971702f218dfbda7b678ad6dff148a080e5e
Zach <sgzkohch@liverpool.ac.uk>
2023-02-21 16:37:35 +0000

    Submitted part 2 on codegrade and it's working

--- assignment1_2.c
+++ assignment1_2.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 int main(void){
-    char string[100];
+    char character;
     int alphabetCount, digitCount, spacesCount, otherCharsCount, i, ascii;
     i = alphabetCount = digitCount = spacesCount = otherCharsCount = 0;
-    scanf("%s", string);
-    while(string[i] != '\0' || string[i] == ' '){
-        ascii = string[i];
+    while(scanf("%c", &character) != EOF){
+        //printf("Tester: %c\n", character);
+        ascii = character;
         if((64<ascii && ascii<91) || (96<ascii && ascii<123)){
+            //printf("alphabet\n");
             alphabetCount++;
 
         }else if(47<ascii && ascii<58){
+            //printf("digit\n");
             digitCount++;
 
         }else if(ascii == 32){
+            //printf("space\n");
             spacesCount++;
 
         }else{
+            //printf("other char\n");
             otherCharsCount++;
 
         }
         i++;
-        printf("%i\n", i);
     }
     printf("%i %i %i %i", alphabetCount, digitCount, spacesCount, otherCharsCount);
 }
